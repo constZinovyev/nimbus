210a12c59f4177ff4679eb0c6152107c6eb01674
k.zinovyev <k.zinovyev@rambler-co.ru>
2017-01-13 17:14:54 +0300

    Added support tvos

---
+++ src/NimbusTVOS/NimbusTVOS.h
@@ -0,0 +1,13 @@
+//
+//  NimbusTVOS.h
+//  NimbusTVOS
+//
+//  Created by k.zinovyev on 13.01.17.
+//  Copyright © 2017 Jeff Verkoeyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NimbusTVOS : NSObject
+
+@end
---
+++ src/NimbusTVOS/NimbusTVOS.m
@@ -0,0 +1,13 @@
+//
+//  NimbusTVOS.m
+//  NimbusTVOS
+//
+//  Created by k.zinovyev on 13.01.17.
+//  Copyright © 2017 Jeff Verkoeyen. All rights reserved.
+//
+
+#import "NimbusTVOS.h"
+
+@implementation NimbusTVOS
+
+@end
